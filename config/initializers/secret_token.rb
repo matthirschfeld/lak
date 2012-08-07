9af814bf434caac2576b4bcd5923fa7cf314a77d
matthirschfeld <matthias.hirschfeld@googlemail.com>
2012-08-07 16:29:23 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lak::Application.config.secret_token = 'f2ae7902d09b0ffb575c6a834b93233fbffcc9cad59d77a8b7145c1d350241d5b93c1fd6173c03f3df6ad43b9f03d19063b027c342e9f6a98b44ca8c2ed766e5'
