e71268a23443266b4c0665206c9b4b02a8f5e7b5
matthirschfeld <matthias.hirschfeld@googlemail.com>
2012-08-09 17:41:46 +0200

    validation works

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,13 @@ class User
   key :alliance_link, String
   key :alliance_status, String
   
+  validates :nick, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+                    
   many :castles
+  
+  before_save { |user| user.email = email.downcase }
 
 end
